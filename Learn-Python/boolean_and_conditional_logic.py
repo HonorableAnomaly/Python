71aa0f1950701803ac3977babd0163031f60ff64
Ricky Argenbright <honorable.anomaly@gmail.com>
2023-02-04 21:18:01 -0800

    Create boolean_and_conditional_logic.py
    + add inputs

---
+++ Learn-Python/boolean_and_conditional_logic.py
@@ -0,0 +1,10 @@
+name = input("Enter your name here: ")
+
+# Input
+# data = input("What's your favorite color?")
+# print("You said " + data)
+
+# Input #2
+print("What's your favorite color?")
+data = input()
+print("You said " + data)
