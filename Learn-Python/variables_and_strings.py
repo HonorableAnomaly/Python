4d39d367f0cf6292b0dd36a74e7b8ed30d5ffe90
Ricky Argenbright <honorable.anomaly@gmail.com>
2023-02-04 19:51:54 -0800

    Update pathnames + create variables_and_strings
    + update pathnames for first_program.py and nums_and_math.py
    + create variables_and_strings.py

---
+++ Learn-Python/variables_and_strings.py
@@ -0,0 +1,45 @@
+pokemon = 3
+
+friends = pokemon
+
+_pokemon = 9
+
+pokemon3 = 3
+
+Pokemon = pokemon
+
+# Python naming conventions
+#   snake_case not camelCase
+#   SNAKE_CASE refers to constants like PI
+#   upper CamelCase refers to class
+#   dunder means __no_touchy__
+#   booleans are UpperCamelCased
+
+
+### Data Types
+# Booleans
+is_active = True
+
+game_over = True
+game_over = False
+
+# Strings
+
+some_string = "8"
+string = "Hello, I am a string!"
+
+# List (ordered/sequenced)
+[1,2,3]
+["a","b","c"]
+
+# Dict (unordered collection)
+{"first_name":"Ricky", "last_name":"Argenbright"}
+
+# Python is dynamically typed and variables can be reassigned as any data type
+
+# None is a Python keyword like null in JS
+first_name = "Daisy"
+age = 30
+child = None
+
+# help(keywords) to search for Python keywords
