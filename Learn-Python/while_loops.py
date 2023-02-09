f9effb12088d633c12ad1a1589fe2a6571bc145d
Ricky Argenbright <honorable.anomaly@gmail.com>
2023-02-08 20:15:23 -0800

    Create while_loops.py
    + add while loop with input to prompt a specific entry
    + add comparison between for and while loop

---
+++ Learn-Python/while_loops.py
@@ -0,0 +1,16 @@
+msg = input("What's the secret password?")
+while msg != "bananas":
+    print("No wai!")
+    msg = input("What's the secret password?")
+print("Of course! Come on in!")
+
+# for and while loop comparison
+# for
+for num in range(1, 11):
+    print(num)
+
+# while
+num = 1
+while num <= 10:
+    print(num)
+    num += 1
